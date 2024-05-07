cf352812f23c974d341cae3351b889ee9a528ad3
howardLiuFrogJump <howard.liu@frog-jump.com>
2024-05-07 12:27:37 +0800

    樣式調整

--- src/App.js
+++ src/App.js
@@ -21,9 +21,20 @@ function App() {
               <div className="card" >
                 <img src="https://images.unsplash.com/photo-1714524719483-324e468bce2d?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">鮮果優格餐
+                  <h6 className="card-title">鮮果優格餐
                     <span className="float-end">NT $ 220</span>
-                  </h5>
+                  </h6>
+                  <button type="button" class="btn btn-outline-primary w-100">加入購物車</button>
+                </div>
+              </div>
+              </div>
+              <div className="col">
+              <div className="card" >
+                <img src="https://plus.unsplash.com/premium_photo-1673240159078-5b644ac78ede?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h6 className="card-title">鮮果優格餐
+                    <span className="float-end">NT $ 220</span>
+                  </h6>
                   <button type="button" class="btn btn-outline-primary w-100">加入購物車</button>
                 </div>
               </div>
@@ -32,20 +43,11 @@ function App() {
               <div className="card" >
                 <img src="https://images.unsplash.com/photo-1714524719483-324e468bce2d?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">鮮果優格餐
-                    <span className="float-end">NT $ 220</span>
-                  </h5>
-                  <button type="button" class="btn btn-outline-primary w-100">加入購物車</button>
-                </div>
-              </div>
-              </div>
-              <div className="col">
-              <div className="card" >
-                <img src="https://images.unsplash.com/photo-1714524719483-324e468bce2d?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
-                <div className="card-body">
-                  <h5 className="card-title">鮮果優格餐
+                  <h6 className="card-title">鮮果優格餐
                     <span className="float-end">NT$220</span>
-                  </h5>
+                  </h6>
+                  <button type="button" class="btn btn-primary w-100">加入購物車</button>
+
                   <button type="button" class="btn btn-outline-primary w-100">加入購物車</button>
                 </div>
               </div>
@@ -54,9 +56,9 @@ function App() {
               <div className="card" >
                 <img src="https://images.unsplash.com/photo-1714524719483-324e468bce2d?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
                 <div className="card-body">
-                  <h5 className="card-title">鮮果優格餐
+                  <h6 className="card-title">鮮果優格餐
                     <span className="float-end">NT $ 220</span>
-                  </h5>
+                  </h6>
                   <button type="button" class="btn btn-outline-primary w-100">加入購物車</button>
                 </div>
               </div>
@@ -72,7 +74,9 @@ function App() {
                       <a href="#">X</a>
                     </td>
                     <td>
-                      <img src="" alt="" />
+                      <img src="https://images.unsplash.com/photo-1714524719483-324e468bce2d?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                       className="table-img"
+                       alt="" />
                     </td>
                     <td>
                       鮮果優格餐
