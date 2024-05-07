f2458f712f84500e44429d35d512884bf5946711
howardLiuFrogJump <howard.liu@frog-jump.com>
2024-05-07 14:18:59 +0800

    產品列表製作

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,50 +1,34 @@
+import productsData from "../assets/productsData";
+
 export default function Products(){
   return(
     <div className="row row-cols-3 g-3">
-    <div className="col">
-    <div className="card" >
-      <img src="https://images.unsplash.com/photo-1714524719483-324e468bce2d?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h6 className="card-title">鮮果優格餐
-          <span className="float-end">NT $ 220</span>
-        </h6>
-        <button type="button" class="btn btn-outline-primary w-100">加入購物車</button>
+      {productsData.map((product)=>{
+        return(
+          <div className="col" key={product.id}>
+            <div className="card" >
+              <img src={product.img} className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <h6 className="card-title">{product.title}
+                  <span className="float-end">NT $ {product.price}</span>
+                </h6>
+                <button type="button" className="btn btn-outline-primary w-100">加入購物車</button>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+      <div className="col">
+        <div className="card" >
+          <img src="https://images.unsplash.com/photo-1714524719483-324e468bce2d?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h6 className="card-title">鮮果優格餐
+              <span className="float-end">NT $ 220</span>
+            </h6>
+            <button type="button" className="btn btn-outline-primary w-100">加入購物車</button>
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-    <div className="col">
-    <div className="card" >
-      <img src="https://plus.unsplash.com/premium_photo-1673240159078-5b644ac78ede?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h6 className="card-title">鮮果優格餐
-          <span className="float-end">NT $ 220</span>
-        </h6>
-        <button type="button" class="btn btn-outline-primary w-100">加入購物車</button>
-      </div>
-    </div>
-    </div>
-    <div className="col">
-    <div className="card" >
-      <img src="https://images.unsplash.com/photo-1714524719483-324e468bce2d?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h6 className="card-title">鮮果優格餐
-          <span className="float-end">NT$220</span>
-        </h6>
-        <button type="button" class="btn btn-outline-primary w-100">加入購物車</button>
-      </div>
-    </div>
-    </div>
-    <div className="col">
-    <div className="card" >
-      <img src="https://images.unsplash.com/photo-1714524719483-324e468bce2d?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h6 className="card-title">鮮果優格餐
-          <span className="float-end">NT $ 220</span>
-        </h6>
-        <button type="button" class="btn btn-outline-primary w-100">加入購物車</button>
-      </div>
-    </div>
-    </div>
-  </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import "./assets/all.scss";
+import "./assets/scss/all.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
